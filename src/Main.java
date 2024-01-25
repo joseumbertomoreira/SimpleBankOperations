2b4de80d1988d958915eb7b4ed0b2afe4eebeec0
stark <stark@localhost.localdomain>
2024-01-25 19:07:06 -0300

    Initial presentation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+       String nome = "Maria de Deus Vieira";
+       String tipoConta = "Conta Corrente";
+       double saldo = 5000;
+       int opcao = 0;
+
+       Scanner read = new Scanner(System.in);
+
+        System.out.println("*************************************************");
+        System.out.println("Dados iniciais do cliente:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de Conta: " + tipoConta);
+        System.out.println("Saldo Inicial: R$ " + saldo);
+        System.out.println("*************************************************");
+
     }
 }
